356e2f1d4d9c076f6ac9a8d29496ff21bbf583ed
Hyejin Cho <jhj191058@gmail.com>
2023-05-12 15:40:02 +0900

    feat:리액트 작업 환경 기본 세팅

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './basic/Header';
+import Main from './Pages/Main';
+import Footer from './basic/Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
