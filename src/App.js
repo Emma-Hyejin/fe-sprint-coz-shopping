ec6b6ce000308f71a8073827fcf7b4e6831e6f20
Hyejin Cho <jhj191058@gmail.com>
2023-05-15 11:26:14 +0900

    feat: Header 햄버거 버튼의 Link를 App에서  Routes 연결

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
+import {BrowserRouter,Routes, Route} from 'react-router-dom';
 import Header from './basic/Header';
 import Main from './Pages/Main';
+import Bookmark from './Pages/Bookmark';
+import ProductLists from './Pages/ProductLists';
 import Footer from './basic/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
+          <Routes>
+            <Route path="/" element={<Main/>}> </Route>
+            <Route path="/products/list" element={<ProductLists/>}></Route>
+            <Route path="/bookmark" element={<Bookmark/>}></Route>
+          </Routes>
+        <Footer/>
+      </div>
+    </BrowserRouter>
   );
 }
 
