6acb1bf1b899c6118e871dbe47b445609418c175
Hyejin Cho <jhj191058@gmail.com>
2023-05-17 09:23:05 +0900

    feat : api fetch로 불러오기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {BrowserRouter,Routes, Route} from 'react-router-dom';
+import {useState, useEffect} from 'react';
 import Header from './basic/Header';
 import Main from './Pages/Main';
 import Bookmark from './Pages/Bookmark';
@@ -8,12 +9,27 @@ import Footer from './basic/Footer';
 
 
 function App() {
+
+  const [list, setLists] = useState([]);
+    
+    useEffect(() => {
+        fetch('http://cozshopping.codestates-seb.link/api/v1/products?count=4')  
+        .then(response =>{
+            if(!response.ok){
+                throw new Error(response.statusText);
+            }
+            return response.json();
+        })
+        .then(json => setLists(json))
+        .catch(error => console.log(error))
+    }, []);
+
   return (
     <BrowserRouter>
       <div className="App">
         <Header/>
           <Routes>
-            <Route path="/" element={<Main/>}> </Route>
+            <Route path="/" element={<Main item={list}/>}> </Route>
             <Route path="/products/list" element={<ProductLists/>}></Route>
             <Route path="/bookmark" element={<Bookmark/>}></Route>
           </Routes>
