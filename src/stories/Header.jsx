2b4102a58d6e2f0c34283050e54a44d0dbcb4f57
Hyejin Cho <jhj191058@gmail.com>
2023-05-26 13:36:52 +0900

    feat : Header storybook 생성 구현

--- src/stories/Header.jsx
+++ src/stories/Header.jsx
@@ -1,59 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
-import { Button } from './Button';
+import styled, {css} from 'styled-components';
+import logo from './assets/Emm.png';
+import menu from './assets/hamberger.png';
 import './header.css';
 
-export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
+const Logoimg = styled.div`
+  width: 60px;
+  height: 60px;
+  background-size: cover;
+  background-image:url(${logo});
+`;
+
+const Menuimg = styled.div`
+  width: 20px;
+  height: 20px;
+  background-size: cover;
+  background-image:url(${menu});
+  
+  &:hover {
+    cursor:pointer;
+  }
+`;
+
+const Lists = styled.li`
+  list-style:none;
+  font-size: 15px;
+  font-weight: 300;
+  padding: 10px 20px;
+
+  &:hover{
+    background-color:#ebeaea;
+    cursor:pointer;
+  }
+`;
+
+export const Header = ({ click }) => (
   <header>
     <div className="storybook-header">
-      <div>
-        <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-          <g fill="none" fillRule="evenodd">
-            <path
-              d="M10 0h12a10 10 0 0110 10v12a10 10 0 01-10 10H10A10 10 0 010 22V10A10 10 0 0110 0z"
-              fill="#FFF"
-            />
-            <path
-              d="M5.3 10.6l10.4 6v11.1l-10.4-6v-11zm11.4-6.2l9.7 5.5-9.7 5.6V4.4z"
-              fill="#555AB9"
-            />
-            <path
-              d="M27.2 10.6v11.2l-10.5 6V16.5l10.5-6zM15.7 4.4v11L6 10l9.7-5.5z"
-              fill="#91BAF8"
-            />
-          </g>
-        </svg>
-        <h1>Acme</h1>
+      <div className="storybook-logo">
+       <Logoimg></Logoimg>
+        <h1>Emm Shopping</h1>
       </div>
-      <div>
-        {user ? (
+      <div className="storybook-menu">
+       <Menuimg/>
+        {click ? (
           <>
-            <span className="welcome">
-              Welcome, <b>{user.name}</b>!
-            </span>
-            <Button size="small" onClick={onLogout} label="Log out" />
+            <div className="dropwdown">
+              <ul className="storybook-lists">
+                <Lists>000님, 안녕하세요.</Lists>
+                <Lists>상품 리스트 페이지</Lists>
+                <Lists>북마크 페이지</Lists>
+              </ul>
+            </div>
           </>
-        ) : (
-          <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
-          </>
-        )}
+        ) : null }
       </div>
     </div>
   </header>
 );
 
-Header.propTypes = {
-  user: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-  }),
-  onLogin: PropTypes.func.isRequired,
-  onLogout: PropTypes.func.isRequired,
-  onCreateAccount: PropTypes.func.isRequired,
-};
-
-Header.defaultProps = {
-  user: null,
-};
--- src/stories/Header.stories.js
+++ src/stories/Header.stories.js
@@ -3,7 +3,6 @@ import { Header } from './Header';
 export default {
   title: 'Example/Header',
   component: Header,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
@@ -11,12 +10,9 @@ export default {
   },
 };
 
-export const LoggedIn = {
+export const ModalOnOff = {
   args: {
-    user: {
-      name: 'Jane Doe',
-    },
+    click: false,
   },
 };
 
-export const LoggedOut = {};
